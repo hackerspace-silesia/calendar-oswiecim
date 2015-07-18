0e7611b21d0d190d5c8f82d991672905e3320671
Firemark <marpiechula@gmail.com>
2015-07-18 16:59:06 +0200

    wtf fix migrate

--- webapp/calendars/migrations/0007_auto_20150718_1629.py
+++ webapp/calendars/migrations/0007_auto_20150718_1629.py
@@ -18,10 +18,5 @@ class Migration(migrations.Migration):
             name='place',
             field=models.CharField(default=datetime.datetime(2015, 7, 18, 16, 29, 46, 206995), max_length=120, verbose_name='Miejsce'),
             preserve_default=False,
-        ),
-        migrations.AlterField(
-            model_name='event',
-            name='point',
-            field=django.contrib.gis.db.models.fields.PointField(srid=4326, blank=True, verbose_name='Miejsce na mapie', null=True),
-        ),
+        )
     ]
