743c6b12cd1f08b9881e93435519f276f447a028
Kamil Gałuszka <galuszkak@gmail.com>
2015-07-18 20:23:58 +0200

    update migrations merge

---
+++ webapp/calendars/migrations/0010_auto_20150718_2023.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('calendars', '0009_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='organizer',
+            name='events',
+            field=models.ManyToManyField(related_name='orgs', to='calendars.Event'),
+        ),
+        migrations.AlterField(
+            model_name='organizer',
+            name='user',
+            field=models.OneToOneField(related_name='organizer', null=True, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
