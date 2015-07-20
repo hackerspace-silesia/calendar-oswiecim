6f4efa20893fb6a24a9ed2f65b334fcf76ac034f
Firemark <marpiechula@gmail.com>
2015-07-20 14:08:01 +0200

    add migration merge

---
+++ webapp/calendars/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('calendars', '0014_auto_20150718_2121'),
+        ('calendars', '0011_auto_20150718_2036'),
+    ]
+
+    operations = [
+    ]
