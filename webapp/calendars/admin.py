e02e88e50c5ae293196d9dd7a197606ffa514c9c
Mariusz Fik <mariusz@fidano.pl>
2016-02-23 18:55:16 +0100

    Use select2 in django admin.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- webapp/calendars/admin.py
+++ webapp/calendars/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from easy_select2 import select2_modelform
 
 # Register your models here.
 from .models import Event, Organizer, Category
@@ -11,12 +12,17 @@ class EventAdmin(admin.ModelAdmin):
               'start_time', 'end_time', 'image', 'orgs', 'user', 'latitude',
               'longitude')
 
+    form = select2_modelform(Event)
+
     class Media:
         css = {
             'screen': ('css/admin.css',)
         }
         js = ('js/vendor/jquery.min.js',)
 
+class OrganizerAdmin(admin.ModelAdmin):
+    form = select2_modelform(Organizer, attrs={'width': '400px'})
+
 admin.site.register(Event, EventAdmin)
-admin.site.register(Organizer)
+admin.site.register(Organizer, OrganizerAdmin)
 admin.site.register(Category)
